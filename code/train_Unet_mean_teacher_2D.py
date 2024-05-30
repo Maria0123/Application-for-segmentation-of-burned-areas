1fc617ebd0f3b7473186d7167ac9b80b3b76b269
Maria Szlasa <maria.szlasa@gmail.com>
2024-05-30 20:08:58 +0200

    feat: enabling MT alghoritm on CaBuArRaw data

--- code/train_Unet_mean_teacher_2D.py
+++ code/train_Unet_mean_teacher_2D.py
@@ -28,9 +28,9 @@ from val_2D import test_single_volume_cbr
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_path', type=str,
-                    default='../data/CaBuAr', help='Name of Experiment')
+                    default='../data/CaBuArRaw', help='Name of Experiment')
 parser.add_argument('--exp', type=str,
-                    default='CaBuAr/Unet', help='experiment_name')
+                    default='CaBuArRaw/MT', help='experiment_name')
 parser.add_argument('--model', type=str,
                     default='unet', help='model_name')
 parser.add_argument('--max_iterations', type=int,
@@ -41,7 +41,7 @@ parser.add_argument('--deterministic', type=int,  default=1,
                     help='whether use deterministic training')
 parser.add_argument('--base_lr', type=float,  default=0.001,
                     help='segmentation network learning rate')
-parser.add_argument('--patch_size', type=list,  default=[512, 512],
+parser.add_argument('--patch_size', type=list,  default=[128, 128],
                     help='patch size of network input')
 parser.add_argument('--seed', type=int,  default=42, help='random seed')
 parser.add_argument('--num_classes', type=int,  default=2,
@@ -62,7 +62,7 @@ parser.add_argument('--consistency_rampup', type=float,
                     default=200.0, help='consistency_rampup')
 
 # loss function
-parser.add_argument('--alpha_hd', type=float,  default=0.6, help='hd95 loss weight')
+parser.add_argument('--alpha_ce', type=float,  default=0.5, help='hd95 loss weight')
 
 args = parser.parse_args()
 
@@ -75,7 +75,7 @@ def patients_to_slices(dataset, patiens_num):
     elif "Prostate" in dataset:
         ref_dict = {"2": 27, "4": 53, "8": 120,
                     "12": 179, "16": 256, "21": 312, "42": 623}
-    elif "CaBuAr" in dataset:
+    elif "CaBuArRaw" in dataset:
         ref_dict = {"3": 15, "7": 70, "13": 150}
     else:
         print("Error")
@@ -115,11 +115,11 @@ def train(args, snapshot_path):
     def worker_init_fn(worker_id):
         random.seed(args.seed + worker_id)
 
-    db_train = CaBuAr(base_dir=args.root_path, split="train", num=None, transform=transforms.Compose([
-        RandomNoise(),
-        RandomFlip(),
-        ToTensor()
-    ]))
+    db_train = CaBuAr(base_dir=args.root_path, split="train", num=None) #, transform=transforms.Compose([
+    #     RandomNoise(),
+    #     RandomFlip(),
+    #     ToTensor()
+    # ]))
     db_val = CaBuAr(base_dir=args.root_path, split="val")
 
     total_slices = len(db_train)
@@ -144,15 +144,16 @@ def train(args, snapshot_path):
     scheduler = StepLR(optimizer, step_size=15, gamma=0.1)
 
     ce_loss = CrossEntropyLoss()                      
-    dice_loss = losses.DiceLoss(num_classes)
-    hd95_loss = losses.HD95Loss(num_classes)
+    dc_hd_loss = losses.DiceHD95Loss(num_classes, args.alpha_ce)
 
     writer = SummaryWriter(snapshot_path + '/log')
     logging.info("{} iterations per epoch".format(len(trainloader)))
 
     iter_num = 0
     max_epoch = max_iterations // len(trainloader) + 1
-    best_performance = 0.0
+    best_performance_ce = 0.0
+    best_performance_hd95 = 1000.0
+    
     iterator = tqdm(range(max_epoch), ncols=70)
     for epoch_num in iterator:
         for i_batch, sampled_batch in enumerate(trainloader):
@@ -178,12 +179,10 @@ def train(args, snapshot_path):
 
             loss_ce   = ce_loss(outputs[:args.labeled_bs], 
                                 label_batch[:args.labeled_bs].squeeze())
-            loss_dice = dice_loss(outputs_soft[:args.labeled_bs], 
+            loss_dc_hd = dc_hd_loss(outputs_soft[:args.labeled_bs], 
                                   label_batch[:args.labeled_bs])
-            loss_hd95 = hd95_loss(outputs_soft[:args.labeled_bs], 
-                                  label_batch[:args.labeled_bs]) if args.alpha_hd > 0 else 0
             
-            supervised_loss = 0.5 * (loss_dice + loss_ce + args.alpha_hd * loss_hd95)
+            supervised_loss = 0.5 * (loss_dc_hd + loss_ce)
             consistency_weight = get_current_consistency_weight(iter_num//150)
             if iter_num < 1000:
                 consistency_loss = 0.0
@@ -205,15 +204,15 @@ def train(args, snapshot_path):
             writer.add_scalar('info/lr', lr_, iter_num)
             writer.add_scalar('info/total_loss', loss, iter_num)
             writer.add_scalar('info/loss_ce', loss_ce, iter_num)
-            writer.add_scalar('info/loss_dice', loss_dice, iter_num)
+            writer.add_scalar('info/loss_dc_hd', loss_dc_hd, iter_num)
             writer.add_scalar('info/consistency_loss',
                               consistency_loss, iter_num)
             writer.add_scalar('info/consistency_weight',
                               consistency_weight, iter_num)
 
             logging.info(
-                'iteration %d : loss : %f, loss_ce: %f, loss_dice: %f, loss_hd95: %f' %
-                (iter_num, loss.item(), loss_ce.item(), loss_dice.item(), loss_hd95.item()))
+                'iteration %d : loss : %f, loss_ce: %f, loss_dc_hd: %f' % 
+                (iter_num, loss.item(), loss_ce.item(), loss_dc_hd.item()))
 
             if iter_num % 20 == 0:
                 image = volume_batch[0, 2:4, :, :]
@@ -232,37 +231,42 @@ def train(args, snapshot_path):
                     metric_i = test_single_volume_cbr(
                         sampled_batch["image"], sampled_batch["label"], model, classes=num_classes)
                     metric_list += np.array(metric_i)
-                metric_list = metric_list / len(db_val)
-                for class_i in range(num_classes-1):
-                    writer.add_scalar('info/val_{}_dice'.format(class_i+1),
-                                      metric_list[class_i, 0], iter_num)
-                    writer.add_scalar('info/val_{}_hd95'.format(class_i+1),
-                                      metric_list[class_i, 1], iter_num)
-                    writer.add_scalar('info/val_{}_jc'.format(class_i+1),
-                                      metric_list[class_i, 2], iter_num)
-                    writer.add_scalar('info/val_{}_f1'.format(class_i+1),
-                                      metric_list[class_i, 3], iter_num)
-                performance = np.mean(metric_list, axis=0)[0]
-
-                mean_hd95 = np.mean(metric_list, axis=0)
-                writer.add_scalar('info/val_mean_dice', performance, iter_num)
-                writer.add_scalar('info/val_mean_hd95', mean_hd95[1], iter_num)
-                writer.add_scalar('info/val_mean_jc', mean_hd95[2], iter_num)
-                writer.add_scalar('info/val_mean_f1', mean_hd95[3], iter_num)
                 
-                if performance > best_performance:
-                    best_performance = performance
-                    save_mode_path = os.path.join(snapshot_path,
-                                                  'iter_{}_dice_{}.pth'.format(
-                                                      iter_num, round(best_performance, 4)))
-                    save_best = os.path.join(snapshot_path,
-                                             '{}_best_model.pth'.format(args.model))
-                    torch.save(model.state_dict(), save_mode_path)
-                    torch.save(model.state_dict(), save_best)
+                metric_list = metric_list / len(db_val)
+                performance = np.mean(metric_list, axis=0)
+
+                writer.add_scalar('info/val_mean_dice', performance[0], iter_num)
+                writer.add_scalar('info/val_mean_hd95', performance[1], iter_num)
+                writer.add_scalar('info/val_mean_jc', performance[2], iter_num)
+                writer.add_scalar('info/val_mean_f1', performance[3], iter_num)
 
                 logging.info(
                     'iteration %d : mean_dice : %f mean_hd95 : %f mean_jc : %f mean_f1 : %f' % 
-                        (iter_num, performance, mean_hd95[1], mean_hd95[2], mean_hd95[3]))
+                        (iter_num, performance[0], performance[1], performance[2], performance[3]))
+                
+                performance_ce = performance[0]
+                if performance_ce > best_performance_ce:
+                    best_performance_ce = performance_ce
+                    save_mode_path = os.path.join(snapshot_path,
+                                                  'iter_{}_ce_{}.pth'.format(
+                                                      iter_num, round(best_performance_ce, 4)))
+                    save_best = os.path.join(snapshot_path,
+                                             '{}_best_model.pth'.format(args.model))
+                    
+                    torch.save(model.state_dict(), save_mode_path)
+                    torch.save(model.state_dict(), save_best)
+
+                performance_hd95 = performance[1]
+                if performance_hd95 < best_performance_hd95:
+                    best_performance_hd95 = performance_hd95
+                    save_mode_path = os.path.join(snapshot_path,
+                                                  'iter_{}_hd95_{}.pth'.format(
+                                                      iter_num, round(best_performance_hd95, 4)))
+                    save_best = os.path.join(snapshot_path,
+                                             '{}_best_model_hd95.pth'.format(args.model))
+                    torch.save(model.state_dict(), save_mode_path)
+                    torch.save(model.state_dict(), save_best)
+
                 model.train()
 
             if iter_num % 300 == 0:
