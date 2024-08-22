da2c390417a901c72834b47fb0429b5f1b53e3e7
Maria Szlasa <maria.szlasa@gmail.com>
2024-08-22 09:08:26 +0200

    adding hrnet and segnet implementation

--- code/networks/net_factory.py
+++ code/networks/net_factory.py
@@ -1,4 +1,6 @@
 import torch
+from networks.hrnet import get_pose_net
+from networks.segnet import SegNet
 from networks.unet import UNet
 import argparse
 from networks.config import get_config
@@ -71,6 +73,10 @@ config = get_config(args)
 def net_factory(net_type="unet", in_chns=1, class_num=3, with_stats=False):
     if net_type == "unet":
         net = UNet(in_chns=in_chns, class_num=class_num, with_stats=with_stats)
+    elif net_type == "segnet":
+        net = SegNet(in_chns=in_chns, class_num=class_num)
+    elif net_type == "hrnet":
+        net = get_pose_net()
     else:
         net = None
 
